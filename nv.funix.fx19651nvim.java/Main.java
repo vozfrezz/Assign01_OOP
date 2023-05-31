26f1f910b781f202e9a28c49b0f492d9a80fdcf4
Tue Khac <tuekhac@gmail.com>
2023-05-31 18:24:23 +0700

    install indent plug and change mapping file

--- nv.funix.fx19651nvim.java/Main.java
+++ nv.funix.fx19651nvim.java/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -34,7 +35,7 @@ public class Main {
                             System.out.println("Invalid option");
                             break;
                     }
-                    
+
                     if (isContinue) {
                         displayOption = DisplayOption(userIDNumber, scanner);
                     }
@@ -46,48 +47,101 @@ public class Main {
     }
 
     public static int ClientOption(Scanner myScanner) {
-        int menuOption;
+        int menuOption = 0;
+        boolean isValidInput = false;
         do {
             System.out.println("+-----------------+--------------------+--------------+");
             System.out.println("| DIGITAL BANKING | FX19651- Beta version             |");
             System.out.println("+-----------------+--------------------+--------------+");
-            System.out.println("| 1.Enter Citizen Identification DisplayOptionber.    |");
+            System.out.println("| 1.Enter Citizen Identification Number.              |");
             System.out.println("| 0.Quit.                                             |");
             System.out.println("+-----------------+--------------------+--------------+");
-
-            System.out.println("Enter option: ");
-            menuOption = myScanner.nextInt();
-            myScanner.nextLine();
-        } while (menuOption != 0 && menuOption != 1);
+            try {
+                System.out.println("Enter option: ");
+                menuOption = myScanner.nextInt();
+                myScanner.nextLine();
+                isValidInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("You can't input a character!");
+                myScanner.nextLine();
+            } catch (Exception e) {
+                System.out.println("Somthing went wrong!!!");
+                myScanner.nextLine();
+            }
+        } while (!isValidInput || menuOption != 0 && menuOption != 1);
         return menuOption;
     }
 
     public static void ValidVerificationCode(Scanner sc) {
+        int menuOption = 0;
+        boolean isValidInput = false;
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         Random rng = new Random();
-        int length = 5;
+        int length = 6;
         boolean match = false;
         char[] text = new char[length];
         char[] confirmtext;
+        int rngNumber, userConfirm;
+
+        System.out.println("Chon chuc nang:");
+        System.out.println("1. Xac thuc 3 ky tu so.");
+        System.out.println("2.Xac thuc 6 ky tu.");
+        System.out.println("0. Thoat chuong trinh.");
         do {
-            for (int i = 0; i < length; i++) {
-                text[i] = characters.charAt(rng.nextInt(characters.length()));
-            }
-            System.out.println("+-----------------+--------------------+--------------+");
-            System.out.print("The verification code: ");
-            for (int i = 0; i < length; i++) {
-                System.out.print(text[i]);
+            try {
+                System.out.println("Enter option: ");
+                menuOption = sc.nextInt();
+                sc.nextLine();
+                isValidInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("You can't input a character!");
+                sc.nextLine();
+            } catch (Exception e) {
+                System.out.println("Somthing went wrong!!!");
+                sc.nextLine();
             }
 
-            System.out.println("\nEnter the code: ");
-            String userInput = sc.next();
-            confirmtext = userInput.toCharArray();
-            if (Arrays.equals(confirmtext, text)) {
-                match = true;
-            } else {
-                System.out.println("Invalid verification code. Please enter again.");
-            }
-        } while (!match);
+        } while (!isValidInput || menuOption < 0 || menuOption > 2);
+
+        switch (menuOption) {
+            case 1:
+                do {
+                    rngNumber = rng.nextInt(1000);
+                    System.out.println("Verification number: " + rngNumber);
+                    System.out.println("Enter the verification number above: ");
+                    userConfirm = sc.nextInt();
+                    sc.nextLine();
+
+                } while (rngNumber != userConfirm);
+
+                break;
+            case 2:
+                do {
+                    for (int i = 0; i < length; i++) {
+                        text[i] = characters.charAt(rng.nextInt(characters.length()));
+                    }
+                    System.out.println("+-----------------+--------------------+--------------+");
+                    System.out.print("The verification code: ");
+                    for (int i = 0; i < length; i++) {
+                        System.out.print(text[i]);
+                    }
+
+                    System.out.println("\nEnter the code: ");
+                    String userInput = sc.next();
+                    confirmtext = userInput.toCharArray();
+                    if (Arrays.equals(confirmtext, text)) {
+                        match = true;
+                    } else {
+                        System.out.println("Invalid verification code. Please enter again.");
+                    }
+                } while (!match);
+
+                break;
+
+            case 0:
+                System.exit(0);
+                break;
+        }
 
     }
 
@@ -105,7 +159,8 @@ public class Main {
     }
 
     public static int DisplayOption(String userIDNumber, Scanner scanner) {
-        int menuOption;
+        int menuOption = 0;
+        boolean isValidInput = false;
         do {
             System.out.println("+-----------------+--------------------+--------------+");
             System.out.println("| DIGITAL BANKING | FX19651- Beta version             |");
@@ -118,20 +173,31 @@ public class Main {
             System.out.println("\t| 2. Kiem tra tuoi,gioi tinh");
             System.out.println("\t| 3. Kiem tra so ngau nhien");
             System.out.println("\t| 0. Thoat");
-            System.out.println("Nhap vao lua chon:");
-            menuOption = scanner.nextInt();
-        } while (menuOption < 0 || menuOption > 3);
-        scanner.nextLine();
+            try {
+                System.out.println("Enter a number:");
+                menuOption = scanner.nextInt();
+                scanner.nextLine();
+                isValidInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid option.");
+                scanner.nextLine();
+            } catch (Exception e) {
+                System.out.println("Something went wrong!");
+                scanner.nextLine();
+            }
+        } while (!isValidInput || menuOption < 0 || menuOption > 3);
         return menuOption;
     }
 
     public static String showRegion(String clientId, ArrayList<String> regionCodes) {
-        String getRegion = clientId.substring(0, 2);
-        String birthPlace = "0", extraRegionCodes;
+        String getRegion = clientId.substring(0, 3);
+        String birthPlace = null, extraRegionCodes;
+        String regionName;
+        int strlen;
         for (String regionCode : regionCodes) {
-            extraRegionCodes = regionCode.substring(0, 2);// lay ra ma tinh trong arraylist
-            int strlen = regionCode.length();
-            String regionName = regionCode.substring(4, strlen);
+            extraRegionCodes = regionCode.substring(0, 3);// lay ra ma tinh trong arraylist
+            strlen = regionCode.length();
+            regionName = regionCode.substring(4, strlen);
             if (getRegion.equals(extraRegionCodes)) {
                 birthPlace = regionName;
                 break;
@@ -165,70 +231,69 @@ public class Main {
     }
 
     public static ArrayList<String> initRegionCode() {
-        ArrayList<String> regionCodes = new ArrayList<>();
-        regionCodes.add("001,Ha Noi");
-        regionCodes.add("002,Ha Giang");
-        regionCodes.add("004,Cao Bang");
-        regionCodes.add("006,Bac Kan");
-        regionCodes.add("008,Tuyen Quang");
-        regionCodes.add("010,Lao Cai");
-        regionCodes.add("011,Dien Bien");
-        regionCodes.add("012,Lai Chau");
-        regionCodes.add("014,Son La");
-        regionCodes.add("015,Yen Bai");
-        regionCodes.add("017,Hoa Binh");
-        regionCodes.add("019,Thai Nguyen");
-        regionCodes.add("020,Lang Son");
-        regionCodes.add("22,Quang Ninh");
-        regionCodes.add("024,Bac Giang");
-        regionCodes.add("025,Phu Tho");
-        regionCodes.add("026,Vinh Phuc");
-        regionCodes.add("027,Bac Ninh");
-        regionCodes.add("030,Hai Duong");
-        regionCodes.add("031,Hai Phong");
-        regionCodes.add("033,Hung Yen");
-        regionCodes.add("034,Thai Binh");
-        regionCodes.add("035,Ha Nam");
-        regionCodes.add("036,Nam Dinh");
-        regionCodes.add("037,Ninh Binh");
-        regionCodes.add("038,Thanh Hoa");
-        regionCodes.add("040,Nghe An");
-        regionCodes.add("042,Ha Tinh");
-        regionCodes.add("044,Quang Binh");
-        regionCodes.add("045,Quang Tri");
-        regionCodes.add("046,Thua Thien-Hue");
-        regionCodes.add("048,Da Nang");
-        regionCodes.add("049,Quang Nam");
-        regionCodes.add("051,Quang Ngai");
-        regionCodes.add("052,Binh Dinh");
-        regionCodes.add("054,Phu Yen");
-        regionCodes.add("056,Khanh Hoa");
-        regionCodes.add("058,Ninh Thuan");
-        regionCodes.add("060,Binh Thuan");
-        regionCodes.add("062,Kon Tum");
-        regionCodes.add("064,Gia Lai");
-        regionCodes.add("066,Dak Lak");
-        regionCodes.add("067,Dak Nong");
-        regionCodes.add("068,Lam Dong");
-        regionCodes.add("070,Binh Phuoc");
-        regionCodes.add("072,Tay Ninh");
-        regionCodes.add("074,Binh Duong");
-        regionCodes.add("075,Dong Nai");
-        regionCodes.add("077,Ba Ria-Vung Tau");
-        regionCodes.add("080,Long An");
-        regionCodes.add("082,Tien Giang");
-        regionCodes.add("083,Ben Tre");
-        regionCodes.add("084,Tra Vinh");
-        regionCodes.add("086,Vinh Long");
-        regionCodes.add("087,Dong Thap");
-        regionCodes.add("089,An Giang");
-        regionCodes.add("091,Kien Giang");
-        regionCodes.add("092,Can Tho");
-        regionCodes.add("093,Hau Giang");
-        regionCodes.add("094,Soc Trang");
-        regionCodes.add("095,Bac Lieu");
-        regionCodes.add("096,Ca Mau");
-
+        ArrayList<String> regionCodes = new ArrayList<>(Arrays.asList(
+                "001,Ha Noi",
+                "002,Ha Giang",
+                "004,Cao Bang",
+                "006,Bac Kan",
+                "008,Tuyen Quang",
+                "010,Lao Cai",
+                "011,Dien Bien",
+                "012,Lai Chau",
+                "014,Son La",
+                "015,Yen Bai",
+                "017,Hoa Binh",
+                "019,Thai Nguyen",
+                "020,Lang Son",
+                "22,Quang Ninh",
+                "024,Bac Giang",
+                "025,Phu Tho",
+                "026,Vinh Phuc",
+                "027,Bac Ninh",
+                "030,Hai Duong",
+                "031,Hai Phong",
+                "033,Hung Yen",
+                "034,Thai Binh",
+                "035,Ha Nam",
+                "036,Nam Dinh",
+                "037,Ninh Binh",
+                "038,Thanh Hoa",
+                "040,Nghe An",
+                "042,Ha Tinh",
+                "044,Quang Binh",
+                "045,Quang Tri",
+                "046,Thua Thien-Hue",
+                "048,Da Nang",
+                "049,Quang Nam",
+                "051,Quang Ngai",
+                "052,Binh Dinh",
+                "054,Phu Yen",
+                "056,Khanh Hoa",
+                "058,Ninh Thuan",
+                "060,Binh Thuan",
+                "062,Kon Tum",
+                "064,Gia Lai",
+                "066,Dak Lak",
+                "067,Dak Nong",
+                "068,Lam Dong",
+                "070,Binh Phuoc",
+                "072,Tay Ninh",
+                "074,Binh Duong",
+                "075,Dong Nai",
+                "077,Ba Ria-Vung Tau",
+                "080,Long An",
+                "082,Tien Giang",
+                "083,Ben Tre",
+                "084,Tra Vinh",
+                "086,Vinh Long",
+                "087,Dong Thap",
+                "089,An Giang",
+                "091,Kien Giang",
+                "092,Can Tho",
+                "093,Hau Giang",
+                "094,Soc Trang",
+                "095,Bac Lieu",
+                "096,Ca Mau"));
         return regionCodes;
     }
 
